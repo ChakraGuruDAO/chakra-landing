8259abf1a9f462264e4e52f5c3ff7db90672210f
raakhme <raakhmee@gmail.com>
2022-05-20 04:10:59 +0300

    fix mobile  boys blocks

--- blocks/nft/complete-and-win.tsx
+++ blocks/nft/complete-and-win.tsx
@@ -12,21 +12,13 @@ import { Boys } from "components/boys";
 export const CompeteWin = () => {
   return (
     <Box bg="dark.100" position="relative">
-      <Box
-        display="none"
-        position="absolute"
-        height="100%"
-        width={{ xl: "40%", md: "50%" }}
-        zIndex="overlay"
-        right="0"
-      ></Box>
       <Container
         display="flex"
-        justifyContent={{ xl: "flex-end", md: "flex-start" }}
-        alignItems="center"
-        height={{ xl: "800px", md: "700px" }}
+        justifyContent={{ xl: "flex-end", base: "flex-start" }}
+        alignItems={{ md: "center", base: "flex-start" }}
+        height={{ xl: "800px", base: "700px" }}
       >
-        {/* <Boys
+        <Boys
           position="absolute"
           top="0"
           bottom="0"
@@ -34,23 +26,25 @@ export const CompeteWin = () => {
           height={{ base: "100%" }}
           left="0"
           right="0"
-        /> */}
+        />
         <VStack
           maxWidth={{ base: "370px" }}
-          justifySelf="center"
+          justifySelf={{ base: "flex-start", md: "center" }}
           // position={{ base: "absolute" }}
           // top={{ base: "50%" }}
           // transform={{ base: "translateY(-50%)" }}
           zIndex="overlay"
+          alignItems="flex-start"
           // background="blackAlpha.600"
           // borderRadius="20px"
           // p="16px"
         >
           <Heading
-            color={{ xl: "white", md: "brand.100" }}
+            color={{ xl: "white", base: "brand.100" }}
             textTransform="uppercase"
+            mt={{ base: "64px", md: 0 }}
             fontWeight={500}
-            fontSize={{ xl: "64px", md: "46px" }}
+            fontSize={{ xl: "64px", md: "46px", base: "26px" }}
             lineHeight="120%"
           >
             COMPETE AND WIN
@@ -58,7 +52,7 @@ export const CompeteWin = () => {
           <Text
             color="white"
             fontWeight={300}
-            fontSize={{ xl: "20px", md: "16px" }}
+            fontSize={{ xl: "20px", base: "16px" }}
             lineHeight="150%"
             mb="24px!important"
           >
@@ -68,7 +62,7 @@ export const CompeteWin = () => {
           <Text
             color="white"
             fontWeight={300}
-            fontSize={{ xl: "20px", md: "16px" }}
+            fontSize={{ xl: "20px", base: "16px" }}
             lineHeight="150%"
           >
             While modifying the metaverse with the Karma dApp, hunt for rare
