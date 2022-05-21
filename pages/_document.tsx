b49bba5aa971385943dd47415a60c52b1b5108eb
Ravil Berishev <quadraticteam@gmail.com>
2022-05-21 13:19:50 +0300

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { ColorModeScript } from "@chakra-ui/react";
+import { Favicon } from "components/favicon";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
@@ -9,6 +10,9 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="utf-8" />
+          <title>Chakra Guru DAO - AR Metaverse</title>
+          <Favicon />
           <link rel="stylesheet" href="/global.css" />
           <script
             type="text/javascript"
